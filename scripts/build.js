719050a668f48551d07f94964eb2eff2d087d9f4
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2021-04-09 10:46:15 +0200

    fix: include gRPC in build command

--- scripts/build.js
+++ scripts/build.js
@@ -6,23 +6,6 @@ const npmrunpath = require("npm-run-path")
 spawnSync(
     "grpc_tools_node_protoc",
     [
-        "--proto_path=./",
-        "--js_out=import_style=commonjs,binary:./",
-        "--grpc_out=grpc_js:./",
-        "./schema/schema.proto",
-        ...process.argv.slice(2)
-    ],
-    {
-        cwd: process.cwd(),
-        shell: true,
-        stdio: "inherit",
-        env: npmrunpath.env()
-    }
-)
-
-spawnSync(
-    "grpc_tools_node_protoc",
-    [
         "--plugin=../../node_modules/.bin/protoc-gen-ts_proto",
         "--ts_proto_out=. ./schema/schema.proto",
         ...process.argv.slice(2)
