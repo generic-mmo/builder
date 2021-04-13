4dbbe2fa91da975727ad29c8cc92e5ad84f4ed86
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2021-04-13 09:59:03 +0200

    feat: added link command

---
+++ scripts/link.js
@@ -0,0 +1,18 @@
+const { spawnSync } = require("child_process")
+const npmrunpath = require("npm-run-path")
+
+
+
+spawnSync(
+    "lerna",
+    [
+        "link",
+        ...process.argv.slice(2)
+    ],
+    {
+        cwd: process.cwd(),
+        shell: true,
+        stdio: "inherit",
+        env: npmrunpath.env()
+    }
+)
